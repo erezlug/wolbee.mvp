fabc145870963fbab122f3017499bc4760773a96
erezlug <40339211+erezlug@users.noreply.github.com>
2024-06-09 23:48:12 +0300

    Update index.js

--- fronted/src/index.js
+++ fronted/src/index.js
@@ -10,6 +10,24 @@ import "./assets/css/line-awesome.min.css";
 import "./assets/scss/main.scss";
 import "./assets/css/material.css";
 
+//
+
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+
+//
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
